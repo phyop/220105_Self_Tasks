04946cc42fd82c15ac977d4e5934cb0ccd587098
phyop <seek1andfind2@gmail.com>
2022-01-14 08:24:04 +0800

    complete!

--- 220107_ramdon_nu_or_blank.py
+++ 220107_ramdon_nu_or_blank.py
@@ -15,6 +15,7 @@ import random
 m, n = 10, 2 # np.array(m*n)
 digits = 100 # 幾位數 = len（10^N）-1
 probability = 0.2
+csvName = 'ramdon.csv'
 
 a = digits*np.random.random((m,n))
 a = np.floor(a) # 取整數
@@ -22,21 +23,21 @@ a = np.floor(a) # 取整數
 index_0 = [i for i in range(a.shape[0])]
 index_1 = [i for i in range(a.shape[1])]
 choose_num = int(probability * m * n) # 其實len(index_0)就是a.shape[0]
-while True:
-    randIndex_0 = np.random.choice(index_0, choose_num) 
-    randIndex_1 = np.random.choice(index_1, choose_num) 
-    randIndex = list(zip(randIndex_0, randIndex_1))
-    # 因為zip結果是回傳迭代器，而for可以直接從迭代器取出，所以上面這樣寫也可以
-    # for i in zip(randIndex_0, randIndex_1):
-    for i in randIndex:
-        a[i[0],i[1]] = np.nan
-    if len(a[np.isnan(a)]) == choose_num: # 非Nan的个数 = len(a[~np.isnan(a)])
-        break
+# while True:
+randIndex_0 = np.random.choice(index_0, choose_num) 
+randIndex_1 = np.random.choice(index_1, choose_num) 
+randIndex = list(zip(randIndex_0, randIndex_1))
+# 因為zip結果是回傳迭代器，而for可以直接從迭代器取出，所以上面這樣寫也可以
+# for i in zip(randIndex_0, randIndex_1):
+for i in randIndex:
+    a[i[0],i[1]] = np.nan
+    # if len(a[np.isnan(a)]) == choose_num: # 非Nan的个数 = len(a[~np.isnan(a)])
+    #     break
 print("probability = ", probability)
 print("number of nan = ", choose_num)
 print(a)
 
-np.savetxt('test.csv', a, delimiter=',',fmt='%f')
+np.savetxt(csvName, a, delimiter=',',fmt='%.1f')
 
 
 # 計算nan的數量
