237ee90a7e58cd28c87f650702890f57ee3a7548
phyop <seek1andfind2@gmail.com>
2022-02-09 22:37:49 +0800

    for i  in range(-1,b+1): b_dict[i] = 100*(i+1)

--- 220209_print_by_num_section.py
+++ 220209_print_by_num_section.py
@@ -8,14 +8,19 @@
 import numpy as np
 import random
 
-def num_exclude(a, number):
+def num_exclude(a, number, b):
+    b_dict = {}
+    for i  in range(-1,b+1):
+        b_dict[i] = 100*(i+1)
+        # {-1:0, 0:100, 1:200, 2:300, 3:400, 4:500}
     count = -1
-    for i in range(number+1):
+    for i in range(b_dict[b-1], b_dict[b]):
         print(f"{i:3d}" ,end=' ') if i not in a else print('  ?', end=' ')
         count += 1
         print() if count%10 == 0 else None
 
 if __name__ == "__main__":
     a = list(np.random.randint(0,500,size=(20)))
+    b = 3
     print(sorted(a))
-    num_exclude(a, 500)
+    num_exclude(a, 500, b)
